70183eb7bf46a4c2fcd9e65a53d8033bcd6eee74
imsiddarth28 <Getlost6757@>
2026-06-23 21:02:14 +0530

    Create new PR for testing AI Review Bot

--- JavaCode6757.java
+++ JavaCode6757.java
@@ -4,7 +4,4 @@ public class ArrayIndexBug {
         int[] numbers = {10, 20, 30, 40, 50};
 
         for (int i = 0; i <= numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
-    }
-}
+            System.out.println(numbers[i])
